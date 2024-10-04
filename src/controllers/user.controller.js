6c02b8971eef75e17a4c4b9a4cd1e11a8bfefefd
krish240317 <krishvaghela1710@gmail.com>
2024-10-05 00:41:56 +0530

    Login And Logut(AccessRefreshToken)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,9 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
-import { User} from "../models/user.model.js"
+import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
 
 const registerUser = asyncHandler(async (req, res) => {
   // get user details from frontend
@@ -29,9 +30,9 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are require ");
   }
 
-  //Check user exsist or not                           step 3
+  //Check user exsist or not   find by both email or username                          step 3
   const exsistedUser = await User.findOne({
-    $or: [{ username }, { email }]
+    $or: [{ username }, { email }],
   });
 
   if (exsistedUser) {
@@ -40,12 +41,16 @@ const registerUser = asyncHandler(async (req, res) => {
   //                                                    Step 4
 
   const avtarLocalPath = req.files?.avatar[0]?.path;
-//   const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  //   const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-let coverImageLocalPath;
-if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
-    coverImageLocalPath = req.files.coverImage[0].path
-}
+  let coverImageLocalPath;
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  ) {
+    coverImageLocalPath = req.files.coverImage[0].path;
+  }
 
   if (!avtarLocalPath) {
     throw new ApiError(400, "Avtar file is require ");
@@ -81,5 +86,111 @@ if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.len
     .status(201)
     .json(new ApiResponse(200, createdUser, "User registered Successfully"));
 });
+//not using asynchandler here as this is our internal method not web request so ok not to use here
 
-export { registerUser };
+const generateAccessAndRefreshToken = async (userID) => {
+  try {
+    const user = await user.findById(userID);
+    const accesstoken = user.generateAccessToken();
+    const refreshtoken = usre.generateRefreshToken();
+
+    //we save refresh token  in DB
+    user.refreshtoken = refreshtoken;
+    //to save rftoken in Db and on calling save all other require filds has to be valid for that not entering other and onlu rf we use validitate
+    await user.save({ validateBeforeSave: false });
+    return { accesstoken, refreshtoken };
+  } catch {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating acccess and refresh tolken ",
+    );
+  }
+};
+
+const loginUser = asyncHandler(async (req, res) => {
+  // req body -> data
+  // username or email
+  //find the user
+  //password check
+  //access and referesh token
+  //send cookie
+
+  // step1
+  const { email, username, password } = req.body;
+  if (!email && !username) {
+    throw new ApiError(400, "username Or email required ");
+  }
+  //step 3
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User Does not Exsist ");
+  }
+
+  //step4
+  const ispasswordvalid = user.isPasswordCorrect(password);
+  if (!ispasswordvalid) {
+    throw new ApiError(401, "Invalid password ");
+  }
+
+  //step 5 generate access token we can do here but we will built another function
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id,
+  );
+  //We do this to get user after giving access and refresh token WHY?????//
+  const loggedInUser = await User.findById(user._id).select("-password -refreshToken")
+  //  step 6 sending cookies
+  //for secure connection only backend can change or modify cookies 
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+  .status(200)
+  .cookie("accessToken", accessToken, options)
+  .cookie("refreshToken", refreshToken, options)
+  .json(
+      new ApiResponse(
+          200, 
+          {
+            //this is for just good practice if user want act or ret
+              user: loggedInUser, accessToken, refreshToken
+          },
+          "User logged In Successfully"
+      )
+  )
+
+});
+
+const logoutUser = asyncHandler(async(req, res) => {
+  //here problem arise is how i will know user who is login ???
+  //we can take input to know email here 
+  await User.findByIdAndUpdate(
+      req.user._id,
+      {
+          $unset: {
+              refreshToken: 1 // this removes the field from document
+          }
+      },
+      {
+          new: true
+      }
+  )
+
+  const options = {
+      httpOnly: true,
+      secure: true
+  }
+
+  return res
+  .status(200)
+  .clearCookie("accessToken", options)
+  .clearCookie("refreshToken", options)
+  .json(new ApiResponse(200, {}, "User logged Out"))
+})
+
+export { registerUser,loginUser,logoutUser };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,23 +1,34 @@
 import { Route, Router } from "express";
-import {registerUser} from "../controllers/user.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 // The Router() function in Express.js creates a new instance of a router object.
-const router =Router()
+const router = Router();
 
-//we want user to upload files while register like avtar and 
+//we want user to upload files while register like avtar and
 router.route("/register").post(
-    //middleware before route 
-    upload.fields([
-        {
-            name:"avatar", //this should be same in front end and backend  
-            maxCount:1    //accept 1 file
-        },
-        {
-            name:"coverImage",
-            maxCount:1
-        }
-    ]),                                                  //to upload multiple files
-    registerUser)
+  //middleware before route
+  upload.fields([
+    {
+      name: "avatar", //this should be same in front end and backend
+      maxCount: 1, //accept 1 file
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]), //to upload multiple files
+  registerUser,
+);
+
+router.route("/login").post(loginUser);
+
+//secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
